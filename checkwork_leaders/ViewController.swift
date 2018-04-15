5ee07343cba1ab69b79341a2074dbb67e46abfae
Iori Minami <iori.minami@gmail.com>
2018-04-15 14:50:52 +0900

    Initial Commit

---
+++ checkwork_leaders/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  checkwork_leaders
+//
+//  Created by 南伊織 on 2018/04/15.
+//  Copyright © 2018年 RigCrunch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
