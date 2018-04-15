4741ff2a60e91baef677f132c4b310d65ca0eb88
Iori Minami <iori.minami@gmail.com>
2018-04-15 18:43:16 +0900

    Full Answer for the Check work

--- checkwork_leaders/ViewController.swift
+++ checkwork_leaders/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var label: UILabel!
+    var wordArray: [String] = ["ぶどう","なし","りんご","いちご"]
+    var index: Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
+        label.text = wordArray[index]
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBAction func next(){
+        index = index + 1
+        if index == 4{
+            index = 0
+        }
+        label.text = wordArray[index]
+    }
 
+    @IBAction func back(){
+        index = index - 1
+        if index <= 0{
+            index = 3
+        }
+        label.text = wordArray[index]
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
